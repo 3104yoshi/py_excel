fe45595cc62452696777ccc359b180a2b64b14da
3104yoshi <3104adgjmp@gmail.com>
2023-08-25 23:31:18 +0900

    add iterate by date

--- pandas_sample/aggregate/aggregate_specific_column.py
+++ pandas_sample/aggregate/aggregate_specific_column.py
@@ -1,22 +1,27 @@
+import datetime
 import pandas as pd
 
-# Load the input Excel file
-input_file = "input/sample.xlsx"
-df = pd.read_excel(input_file)
+start_date = datetime.date(2019, 1, 1)
+end_date = datetime.date(2019, 1, 31)
 
-# Drop unnecessary columns
-# df = df.drop(columns=['nouse', 'nouse.1', 'nouse.2'])
+for date_delta in range((end_date - start_date).days + 1):
+    # Load the input Excel file
+    input_file = "input/sample.xlsx"
+    df = pd.read_excel(input_file)
 
-# Pivot the DataFrame to reshape it
-pivot_df = df.pivot(index='name', columns='item', values='price')
-pivot_df = pivot_df.sort_index()
+    # Drop unnecessary columns
+    # df = df.drop(columns=['nouse', 'nouse.1', 'nouse.2'])
 
-# Reset index and rename columns
-pivot_df = pivot_df.reset_index()
-pivot_df.columns.name = None
+    # Pivot the DataFrame to reshape it
+    pivot_df = df.pivot(index='name', columns='item', values='price')
+    pivot_df = pivot_df.sort_index()
 
-# Save the output Excel file
-output_file = "output/sample.xlsx"
-pivot_df.to_excel(output_file, index=False)
+    # Reset index and rename columns
+    pivot_df = pivot_df.reset_index()
+    pivot_df.columns.name = None
+
+    # Save the output Excel file
+    output_file = "output/"+str(start_date+datetime.timedelta(date_delta))+"sample.xlsx"
+    pivot_df.to_excel(output_file, index=False)
 
 print("Output generated successfully using pandas!")
